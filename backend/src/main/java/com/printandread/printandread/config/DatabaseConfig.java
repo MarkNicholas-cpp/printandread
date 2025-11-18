d03959060654d1eb23fb10caf72a40edd56022b8
MarkNicholas-cpp <marknicholas2002112@gmail.com>
2025-11-18 20:59:08 +0530

    Remove unused import

--- backend/src/main/java/com/printandread/printandread/config/DatabaseConfig.java
+++ backend/src/main/java/com/printandread/printandread/config/DatabaseConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 
 import javax.sql.DataSource;
